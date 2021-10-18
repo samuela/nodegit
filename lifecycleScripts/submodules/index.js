971ba2de3b665f1d27a4a05c9d0c2a9fed042942
Samuel Ainsworth <skainsworth@gmail.com>
2021-10-17 18:29:52 -0700

    lifecycleScripts/submodules/index.js: async/await
    See https://github.com/nodegit/nodegit/issues/1867.

--- lifecycleScripts/submodules/index.js
+++ lifecycleScripts/submodules/index.js
@@ -1,82 +1,61 @@
-var path = require("path");
-var rootDir = path.join(__dirname, "../..");
+const path = require("path");
+const rootDir = path.join(__dirname, "../..");
 
-var gitExecutableLocation = require(
-  path.join(rootDir, "./utils/gitExecutableLocation")
-);
-var submoduleStatus = require("./getStatus");
+const gitExecutableLocation = require(path.join(
+  rootDir,
+  "./utils/gitExecutableLocation"
+));
+const submoduleStatus = require("./getStatus");
 
-var exec = require(path.join(rootDir, "./utils/execPromise"));
+const exec = require(path.join(rootDir, "./utils/execPromise"));
 
-module.exports = function submodules() {
-  return gitExecutableLocation()
-    .catch(function() {
-      console.error("[nodegit] ERROR - Compilation of NodeGit requires git " +
-        "CLI to be installed and on the path");
+module.exports = async function submodules() {
+  try {
+    await gitExecutableLocation();
+  } catch (_) {
+    console.error(
+      "[nodegit] ERROR - Compilation of NodeGit requires git " +
+        "CLI to be installed and on the path"
+    );
 
-      throw new Error("git CLI is not installed or not on the path");
-    })
-    .then(function() {
-      console.log("[nodegit] Checking submodule status");
-      return submoduleStatus();
-    })
-    .then(function(statuses) {
-      function printSubmodule(submoduleName) {
-        console.log("\t" + submoduleName);
-      }
+    throw new Error("git CLI is not installed or not on the path");
+  }
 
-      var dirtySubmodules = statuses
-      .filter(function(status) {
-        return status.workDirDirty && !status.needsInitialization;
-      })
-      .map(function(dirtySubmodule) {
-        return dirtySubmodule.name;
-      });
+  console.log("[nodegit] Checking submodule status");
+  const statuses = await submoduleStatus();
 
-      if (dirtySubmodules.length) {
-        console.error(
-          "[nodegit] ERROR - Some submodules have uncommited changes:"
-        );
-        dirtySubmodules.forEach(printSubmodule);
-        console.error(
-          "\nThey must either be committed or discarded before we build"
-        );
+  function printSubmodule(submoduleName) {
+    console.log("\t" + submoduleName);
+  }
 
-        throw new Error("Dirty Submodules: " + dirtySubmodules.join(" "));
-      }
+  const dirtySubmodules = statuses
+    .filter((status) => status.workDirDirty && !status.needsInitialization)
+    .map((dirtySubmodule) => dirtySubmodule.name);
 
-      var outOfSyncSubmodules = statuses
-        .filter(function(status) {
-          return status.onNewCommit && !status.needsInitialization;
-        })
-        .map(function(outOfSyncSubmodule) {
-          return outOfSyncSubmodule.name;
-        });
+  if (dirtySubmodules.length) {
+    console.error("[nodegit] ERROR - Some submodules have uncommited changes:");
+    dirtySubmodules.forEach(printSubmodule);
+    console.error(
+      "\nThey must either be committed or discarded before we build"
+    );
 
-      if (outOfSyncSubmodules.length) {
-        console.warn(
-          "[nodegit] WARNING - Some submodules are pointing to an new commit:"
-        );
-        outOfSyncSubmodules.forEach(printSubmodule);
-        console.warn("\nThey will not be updated.");
-      }
+    throw new Error("Dirty Submodules: " + dirtySubmodules.join(" "));
+  }
 
-      return statuses
-        .filter(function(status) {
-          return !status.onNewCommit;
-        })
-        .reduce(function(chainPromise, submoduleToUpdate) {
-          return chainPromise
-            .then(function() {
-              console.log(
-                "[nodegit] Initializing submodule",
-                submoduleToUpdate.name
-              );
-              return exec(
-                "git submodule update --init --recursive " +
-                submoduleToUpdate.name
-              );
-            });
-        }, Promise.resolve());
-    });
+  const outOfSyncSubmodules = statuses
+    .filter((status) => status.onNewCommit && !status.needsInitialization)
+    .map((outOfSyncSubmodule) => outOfSyncSubmodule.name);
+
+  if (outOfSyncSubmodules.length) {
+    console.warn(
+      "[nodegit] WARNING - Some submodules are pointing to an new commit:"
+    );
+    outOfSyncSubmodules.forEach(printSubmodule);
+    console.warn("\nThey will not be updated.");
+  }
+
+  for (const status of statuses.filter((s) => !s.onNewCommit)) {
+    console.log("[nodegit] Initializing submodule", status.name);
+    await exec("git submodule update --init --recursive " + status.name);
+  }
 };
