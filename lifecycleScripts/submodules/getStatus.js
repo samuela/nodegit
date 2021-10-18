ee243f69da551255e0daf163087dbe4ce1f384c1
Samuel Ainsworth <skainsworth@gmail.com>
2021-10-17 18:27:22 -0700

    lifecycleScripts/submodules/getStatus.js: async/await
    See https://github.com/nodegit/nodegit/issues/1867.

--- lifecycleScripts/submodules/getStatus.js
+++ lifecycleScripts/submodules/getStatus.js
@@ -1,50 +1,46 @@
-var path = require("path");
-var rootDir = path.join(__dirname, "../..");
-var exec = require(path.join(rootDir, "./utils/execPromise"));
+const path = require("path");
+const rootDir = path.join(__dirname, "../..");
+const exec = require(path.join(rootDir, "./utils/execPromise"));
 
-module.exports = function getStatus() {
-  return exec("git submodule status", { cwd: rootDir})
-    .then(function(stdout) {
-      if (!stdout) {
-        // In the case where we pull from npm they pre-init the submodules for
-        // us and `git submodule status` returns empty-string. In that case
-        // we'll just assume that we're good.
-        return Promise.resolve([]);
-      }
+module.exports = async function getStatus() {
+  try {
+    const stdout = await exec("git submodule status", { cwd: rootDir });
+    if (!stdout) {
+      // In the case where we pull from npm they pre-init the submodules for
+      // us and `git submodule status` returns empty-string. In that case
+      // we'll just assume that we're good.
+      return [];
+    }
 
-      function getStatusPromiseFromLine(line) {
-        var lineSections = line.trim().split(" ");
-        var onNewCommit = !!~lineSections[0].indexOf("+");
-        var needsInitialization = !!~lineSections[0].indexOf("-");
-        var commitOid = lineSections[0].replace("+", "").replace("-", "");
-        var name = lineSections[1];
+    async function getStatusPromiseFromLine(line) {
+      const lineSections = line.trim().split(" ");
+      const onNewCommit = !!~lineSections[0].indexOf("+");
+      const needsInitialization = !!~lineSections[0].indexOf("-");
+      const commitOid = lineSections[0].replace("+", "").replace("-", "");
+      const name = lineSections[1];
 
-        return exec("git status", { cwd: path.join(rootDir, name)})
-          .then(function(workDirStatus) {
-            return {
-              commitOid: commitOid,
-              onNewCommit: onNewCommit,
-              name: name,
-              needsInitialization: needsInitialization,
-              workDirDirty: !~workDirStatus
-                .trim()
-                .split("\n")
-                .pop()
-                .indexOf("nothing to commit")
-            };
-          });
-      }
+      const workDirStatus = await exec("git status", {
+        cwd: path.join(rootDir, name),
+      });
+      return {
+        commitOid: commitOid,
+        onNewCommit: onNewCommit,
+        name: name,
+        needsInitialization: needsInitialization,
+        workDirDirty: !~workDirStatus
+          .trim()
+          .split("\n")
+          .pop()
+          .indexOf("nothing to commit"),
+      };
+    }
 
-      return Promise.all(stdout
-        .trim()
-        .split("\n")
-        .map(getStatusPromiseFromLine)
-      );
-    })
-    .catch(function() {
-      // In the case that NodeGit is required from another project via npm we
-      // won't be able to run submodule commands but that's ok since the
-      // correct version of libgit2 is published with nodegit.
-      return Promise.resolve([]);
-    });
+    return await Promise.all(
+      stdout.trim().split("\n").map(getStatusPromiseFromLine)
+    );
+  } catch (_) {
+    // In the case that NodeGit is required from another project via npm we
+    // won't be able to run submodule commands but that's ok since the
+    // correct version of libgit2 is published with nodegit.
+  }
 };
